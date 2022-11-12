e22ce88b1811fd0ef448e4523fc6d6998253870c
seungyeonchoi <seungyeonchoi@worksmobile.com>
2022-11-12 23:23:35 +0900

    로그 추적기 V1- 적용
    요구사항 중 일부 구현
    - 모든 public 메서드의 호출과 응답 정보를 로그로 출력
    - 애플리케이션의 흐름을 변경하면 안됨
      - 로그를 남긴다고 해서 비즈니스 로직의 동작에 영향을 주면 안됨
    - 메서드 호출에 걸린 시간
    - 정상 흐름과 예외 흐름 구분
      - 예외 발생 시 예외 정보가 남아야 함

---
+++ src/main/java/hello/advanced/app/v1/OrderControllerV1.java
@@ -0,0 +1,32 @@
+package hello.advanced.app.v1;
+
+
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.hellotrace.HelloTraceV1;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderControllerV1 {
+
+    private final OrderServiceV1 orderService;
+    private final HelloTraceV1 trace;
+
+    @GetMapping("/v1/request")
+    public String request(String itemId) {
+        TraceStatus status = null;
+        try {
+            status = trace.begin("OrderController.request()");
+            orderService.orderItem(itemId);
+            trace.end(status);
+            return "ok";
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e; //예외를 던지지 않으면, 이 catch문에서 에러를 먹기 때문에, 비즈니스 로직의 흐름을 바꾸게 되는 것임
+        }
+    }
+
+
+}
---
+++ src/main/java/hello/advanced/app/v1/OrderRepositoryV1.java
@@ -0,0 +1,39 @@
+package hello.advanced.app.v1;
+
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.hellotrace.HelloTraceV1;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV1 {
+
+    private final HelloTraceV1 trace;
+
+    public void save(String itemId)  {
+        TraceStatus status = null;
+        try {
+            status = trace.begin("OrderController.save()");
+
+            // 저장 로직
+            if (itemId.equals("ex")) {
+                throw new IllegalStateException("예외 발생!");
+            }
+            sleep(1000);
+
+            trace.end(status);
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e; //예외를 던지지 않으면, 이 catch문에서 에러를 먹기 때문에, 비즈니스 로직의 흐름을 바꾸게 되는 것임
+        }
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
