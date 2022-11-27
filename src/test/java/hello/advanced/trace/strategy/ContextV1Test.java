224c3f6e8513f8cf56f2a78c22d9b0a5ed5745ca
seungyeonchoi <seungyeonchoi@worksmobile.com>
2022-11-27 22:36:39 +0900

    전략 패턴 - 예제1
    - 전략패턴 확인용 인터페이스, 클래스 추가

--- src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
+++ src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
@@ -30,4 +30,18 @@ public class ContextV1Test {
         long resultTime = endTime - startTime;
         log.info("resultTime={}", resultTime);
     }
+
+    /**
+     * 전략 패턴 사용
+     */
+    @Test
+    void strategyV1() {
+        StrategyLogic1 strategyLogic1 = new StrategyLogic1();
+        ContextV1 context1 = new ContextV1(strategyLogic1);
+        context1.execute();
+
+        StrategyLogic2 strategyLogic2 = new StrategyLogic2();
+        ContextV1 context2 = new ContextV1(strategyLogic2);
+        context2.execute();
+    }
 }
---
+++ src/test/java/hello/advanced/trace/strategy/Strategy.java
@@ -0,0 +1,6 @@
+package hello.advanced.trace.strategy;
+
+// 변하는 알고리즘 역할
+public interface Strategy {
+    void call();
+}
