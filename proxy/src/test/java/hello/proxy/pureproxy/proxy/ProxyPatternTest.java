2814e59a1223bea870ad18156e1ad3cee6d8c2a4
seungyeonchoi <seungyeonchoi@worksmobile.com>
2022-12-07 00:21:18 +0900

    프록시 패턴 - 예제 코드2
    - 프록시를 적용한 예제 코드 작성
      - 캐싱 역할을 하는 프록시 객체가 도입되어서, 실제 객체 호출은 1번만 되고 있음
    - 런타임 객체 의존 관계
      - 클라이언트 -> 프록시객체 -> 리얼 객체

--- proxy/src/test/java/hello/proxy/pureproxy/proxy/ProxyPatternTest.java
+++ proxy/src/test/java/hello/proxy/pureproxy/proxy/ProxyPatternTest.java
@@ -1,5 +1,6 @@
 package hello.proxy.pureproxy.proxy;
 
+import hello.proxy.pureproxy.proxy.code.CacheProxy;
 import hello.proxy.pureproxy.proxy.code.ProxyPatternClient;
 import hello.proxy.pureproxy.proxy.code.RealSubject;
 import org.junit.jupiter.api.Test;
@@ -18,5 +19,21 @@ public class ProxyPatternTest {
         //00:17:34.546 [main] INFO hello.proxy.pureproxy.proxy.code.RealSubject - 실제 객체 호출
         //00:17:35.549 [main] INFO hello.proxy.pureproxy.proxy.code.RealSubject - 실제 객체 호출
         //00:17:36.554 [main] INFO hello.proxy.pureproxy.proxy.code.RealSubject - 실제 객체 호출
+    }
+
+    @Test
+    void cacheProxyTest() {
+        RealSubject realSubject = new RealSubject();
+        CacheProxy cacheProxy = new CacheProxy(realSubject);
+        ProxyPatternClient client = new ProxyPatternClient(cacheProxy);
+
+        client.execute();
+        client.execute();
+        client.execute();
+
+        //00:17:08.998 [main] INFO hello.proxy.pureproxy.proxy.code.CacheProxy - 프록시 호출
+        //00:17:08.999 [main] INFO hello.proxy.pureproxy.proxy.code.RealSubject - 실제 객체 호출
+        //00:17:10.004 [main] INFO hello.proxy.pureproxy.proxy.code.CacheProxy - 프록시 호출
+        //00:17:10.005 [main] INFO hello.proxy.pureproxy.proxy.code.CacheProxy - 프록시 호출
     }
 }
