cbcb62d4e5443a29e900fb97e16c592907c41c1f
seungyeonchoi <seungyeonchoi@worksmobile.com>
2022-12-06 23:23:48 +0900

    예제 프로젝트 만들기 v2
    - Controller, Servcie, Repository를 구체 클랙스 작성
    - AppV2Config 을 이용해 스프링 빈으로 수동 등록

--- proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -1,11 +1,12 @@
 package hello.proxy;
 
 import hello.proxy.config.AppV1Config;
+import hello.proxy.config.AppV2Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
-@Import(AppV1Config.class) //이 설정파일을 읽어서, 스프링 빈을 등록함(수동 빈 설정)
+@Import({AppV1Config.class, AppV2Config.class}) //이 설정파일을 읽어서, 스프링 빈을 등록함(수동 빈 설정)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //hello.proxy.app 하위만 컴포넌트 스캔 대상으로 지정 (v1,v2,v3에 따라 빈 등록방법이 달라져서 일부러 이렇게 설정)
 public class ProxyApplication {
 
