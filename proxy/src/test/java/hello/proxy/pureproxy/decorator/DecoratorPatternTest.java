9bbeaa29d3ee71842bb900309ebbf2316150ddcd
seungyeonchoi <seungyeonchoi@worksmobile.com>
2022-12-07 00:32:53 +0900

    데코레이터 패턴 - 예제 코드2
    - 데코레이터 패턴에서 프록시 적용 예제(문자열 꾸며주기)
      - 클라이언트의 수정 없이, 부가 기능 추가
    - 런타임 시 의존관계
      - 클라이언트 -> 프록시 객체 -> 리얼 객체

--- proxy/src/test/java/hello/proxy/pureproxy/decorator/DecoratorPatternTest.java
+++ proxy/src/test/java/hello/proxy/pureproxy/decorator/DecoratorPatternTest.java
@@ -1,6 +1,7 @@
 package hello.proxy.pureproxy.decorator;
 
 import hello.proxy.pureproxy.decorator.code.DecoratorPatternClient;
+import hello.proxy.pureproxy.decorator.code.MessageDecorator;
 import hello.proxy.pureproxy.decorator.code.RealComponent;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
@@ -16,5 +17,18 @@ public class DecoratorPatternTest {
         client.execute();
         //00:30:22.714 [main] INFO hello.proxy.pureproxy.decorator.code.RealComponent - RealComponent 실행
         //00:30:22.715 [main] INFO hello.proxy.pureproxy.decorator.code.DecoratorPatternClient - result=data
+    }
+
+    @Test
+    void decorator1() {
+        RealComponent realComponent = new RealComponent();
+        MessageDecorator messageDecorator = new MessageDecorator(realComponent);
+        DecoratorPatternClient client = new DecoratorPatternClient(messageDecorator);
+
+        client.execute();
+        //00:30:11.564 [main] INFO hello.proxy.pureproxy.decorator.code.MessageDecorator - MessageDecorator 실행
+        //00:30:11.565 [main] INFO hello.proxy.pureproxy.decorator.code.RealComponent - RealComponent 실행
+        //00:30:11.565 [main] INFO hello.proxy.pureproxy.decorator.code.MessageDecorator - MessageDecorator 꾸미기 적용 전=data, 적용 후=****data****
+        //00:30:11.566 [main] INFO hello.proxy.pureproxy.decorator.code.DecoratorPatternClient - result=****data****
     }
 }
